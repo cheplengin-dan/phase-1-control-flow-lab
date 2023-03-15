eaf3883ed477578ba1a63ee6328c84c4179ec843
Duncan <cheplenginduncan@gmail.com>
2023-03-15 12:23:00 +0300

    Completed control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet( feet){
+
+if ( feet == 199 ) {
+  return 'This one is on me!';
+
+} else  if ( feet == 2001 ) {
+  return 'I will gladly take your thirty bucks.';
+
+} else if ( feet == 2501 ) {
+  return 'No can do.';
+}
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+let city1 = 'NYC';
+let city2 = 'Pittsburgh';
+
+function ternaryCheckCity(city){
+
+
+ const checkCity = city === 'NYC'  ?  'Ok, sounds good.' : 'No go.';
+
+ return checkCity;
+
+
   // Write your code here!
+
 }
 
-function switchOnCharmFromTip(){
+
+
+
+function switchOnCharmFromTip( Tip){
+  switch ( Tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+
+    case 'thanks for everything':
+      return 'Bye.';
+      break;
+  }
+
   // Write your code here!
+
 }
